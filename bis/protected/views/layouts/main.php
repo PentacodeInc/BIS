faa3a12c9dd3cd02c03ecc7426fb18488be90c4a
abigail aligan <ubiealigan@gmail.com>
2015-02-14 21:02:36 +0800

    Slideshow but not responsive.

--- bis/protected/views/layouts/main.php
+++ bis/protected/views/layouts/main.php
@@ -16,6 +16,9 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css">
     
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:600italic,400,600,700,800' rel='stylesheet' type='text/css'>
+    
+    <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery-1.9.1.min.js"></script>
+    <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/jssor.slider.min.js"></script>
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>

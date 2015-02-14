a81eb81ec6ebcdd54b62c338f09ccec45d32d0fd
abigail aligan <ubiealigan@gmail.com>
2015-02-14 21:23:44 +0800

    Slideshow responsive
    too big maybe?

--- bis/protected/views/site/index.php
+++ bis/protected/views/site/index.php
@@ -1,32 +1,26 @@
 <script>
-    jssor_slider1_starter = function (containerId) {
-        var options = {
-            $DragOrientation: 3,                                
-            $SlideDuration: 500,
-            $ArrowNavigatorOptions: {
-                $Class: $JssorArrowNavigator$,            
-                $ChanceToShow: 2,
-                $AutoCenter: 2,
-                $Steps: 1                                       
-            }
-        };
-        var jssor_slider1 = new $JssorSlider$(containerId, options);
-    };
+$(function () {
+      $(".rslides").responsiveSlides({
+        auto: false,
+        pager: false,
+        nav: true,
+        speed: 500,
+        namespace: "callbacks",
+      });
+
+    });
 </script>
 
 <div id="slideshow">
-<div id="slider1_container" style="position: relative; width: 1100px; height: 500px; overflow: hidden;margin:auto;">
-        <div u="slides" style="position: relative; width:1100px; height: 500px; overflow: hidden; ">
-            <div><img u="image" src="<?php echo Yii::app()->request->baseUrl; ?>/images/blue.jpg" /></div>
-            <div><img u="image" src="<?php echo Yii::app()->request->baseUrl; ?>/images/red.jpg" /></div>
-            <div><img u="image" src="<?php echo Yii::app()->request->baseUrl; ?>/images/purple.jpg" /></div>
-        </div>
-        <span u="arrowleft" class="jssora01l" style="width: 45px; height: 45px; top: 123px; left: 8px;"></span>
-        <span u="arrowright" class="jssora01r" style="width: 45px; height: 45px; top: 123px; right: 8px"></span>
-        <script>
-            jssor_slider1_starter('slider1_container');
-        </script>
-    </div></div>
+<div class="callbacks_container">
+<ul class="rslides">
+  <li><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/blue.jpg" alt=""></li>
+  <li><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/red.jpg" alt=""></li>
+  <li><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/purple.jpg" alt=""></li>
+</ul>
+</div>
+    
+</div>
 
 <div id="home">
     

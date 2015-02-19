17e9cb2bc9b7f30a4d64d3dcf0cbfe698f88946a
abigail aligan <ubiealigan@gmail.com>
2015-02-20 00:48:37 +0800

    upload file in the folder for the downloadable files

--- bis/protected/views/downloadableFiles/index.php
+++ bis/protected/views/downloadableFiles/index.php
@@ -14,11 +14,6 @@ $this->menu=array(
 
 <h1>Downloadable Files</h1>
 
-<?php //$this->widget('zii.widgets.CListView', array(
-	//'dataProvider'=>$dataProvider,
-	//'itemView'=>'_view',
-//)); ?>
-
 <?php $this->widget('zii.widgets.grid.CGridView', array(
 	'id'=>'downloadable-files-grid',
 	'dataProvider'=>$dataProvider,
@@ -28,7 +23,6 @@ $this->menu=array(
             'value' => 'CHtml::link($data->filename,Yii::app()->request->baseUrl."/images/downloadable/".$data->filename,array("target"=>"_blank"))',
             'type' => 'raw',
         ),
-        //'filename',
         'name'
     )
 )); ?>

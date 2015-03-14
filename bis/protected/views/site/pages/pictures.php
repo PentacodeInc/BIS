96adfb0a7cef1efce135520eae1224a41e654060
jklm <jkmariano14@gmail.com>
2015-03-14 15:18:54 -0700

    Added OFFICIAL, Facebook Album API
    Added OFFICIAL in DB schema
    Photo Gallery display Facebook page albums

--- bis/protected/views/site/pages/pictures.php
+++ bis/protected/views/site/pages/pictures.php
@@ -1,38 +1,33 @@
 <div id="about" class="pictures">
     <div id="white">
         <div class="aboutTitle">Photo Gallery</div>
-        
+
         <div class="album">
-            <div class="item">
-            <div class="pic"><a href="https://www.facebook.com/media/set/?set=a.561592620550162.1073741839.515313718511386&type=3" target="_blank">
-                <img src="https://scontent-hkg.xx.fbcdn.net/hphotos-frc3/v/t1.0-9/1044783_561592660550158_1729781484_n.jpg?oh=a5147df85dcd853ec9a118e6101e46ac&oe=5550B25D"/></a>
-            </div>
-            <div class="name">SK Chairman Seminar</div>
-            </div>
-            <div class="item">
-            <div class="pic"><a href="https://www.facebook.com/media/set/?set=a.561278103914947.1073741838.515313718511386&type=3" target="_blank">
-                <img src="https://scontent-hkg.xx.fbcdn.net/hphotos-prn2/v/t1.0-9/943237_561278157248275_1454499155_n.jpg?oh=3b3419311c8c237fbc5a5214874c4adf&oe=558E439D"/></a>
-            </div>
-            <div class="name">STAR 2013 @ DLS-CSB</div>
-            </div>
-            <div class="item">
-            <div class="pic"><a href="https://www.facebook.com/media/set/?set=a.561276513915106.1073741837.515313718511386&type=3" target="_blank">
-                <img src="https://scontent-hkg.xx.fbcdn.net/hphotos-ash2/v/t1.0-9/944155_561276753915082_1816939085_n.jpg?oh=30fc5391e35d8bb3487ad347d73e5b90&oe=55890CE2"/></a>
-            </div>
-            <div class="name">KABAKA Volleyball Tournament</div>
-            </div>
-            <div class="item">
-            <div class="pic"><a href="https://www.facebook.com/media/set/?set=a.515688941807197.1073741834.515313718511386&type=3" target="_blank">
-                <img src="https://scontent-hkg.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/563446_515689245140500_1917775984_n.jpg?oh=16b6c65399ccbbae31e85353b50cc78a&oe=554A506A"/></a>
-            </div>
-            <div class="name">Coastal Clean Up: Manila Bay</div>
-            </div>
-            <div class="item">
-            <div class="pic"><a href="https://www.facebook.com/media/set/?set=a.515677391808352.1073741831.515313718511386&type=3" target="_blank">
-                <img src="https://fbcdn-sphotos-f-a.akamaihd.net/hphotos-ak-prn2/v/t1.0-9/63277_515679481808143_2112862807_n.jpg?oh=2b883e3c4599452120f64460b7d78edc&oe=55558ECA&__gda__=1435069060_01f1d631148bd58febf326a52a9c6213"/></a>
-            </div>
-            <div class="name">Street Rehabilitation: Vito Cruz and Dominga St.</div>
-            </div>
+			<?php
+				// albums to be excluded
+				$exclude = ['Timeline Photos', 'Untitled Album', 'Profile Pictures', 'Cover Photos'];
+				
+				$foo = file_get_contents("http://graph.facebook.com/515313718511386/albums");
+				$albums = json_decode($foo, true);
+				
+				// loop albums
+				foreach($albums['data'] as $album){
+					if (!in_array($album['name'], $exclude)) {
+						echo "<div class='item'>";
+						echo "<div class='pic'>";
+						echo "<a href='".$album['link']."' target='blank'>";
+						
+						$bar = file_get_contents("http://graph.facebook.com/".$album['cover_photo']);
+						$cover = json_decode($bar, true);
+						
+						echo "<img src='".$cover['source']."' />";
+						echo "</a>";
+						echo "</div>";
+						echo "<div class='name'>".$album['name']."</div>";
+						echo "</div>";
+					}
+				}
+			?>
         </div>
         
     </div>
